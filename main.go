4e38e01283adc8552045d1ae75c7cab5a60f5017
Ritesh Sinha <riteshsinha@Riteshs-MacBook-Pro.local>
2016-08-29 17:37:27 +0100

    Updated

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", sayHello)
-	http.ListenAndServe("localhost:5050", nil)
+	http.ListenAndServe(":5050", nil)
 }
 
 const (
-	msgHello = `
-<?xml version="1.0" encoding="UTF-8"?>
+	msgHello = `<?xml version="1.0" encoding="UTF-8"?>
 <Response> 
-<Say> Hello Friend </Say>
+<Say> What's the password? </Say>
 </Response> `
 )
 
